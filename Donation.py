574362e501675b5209420df5323825a3acd3d040
Rushikesh <147487623+rushikesh1437@users.noreply.github.com>
2024-10-30 16:03:57 +0530

    Uploading code: Donation.py

---
+++ Donation.py
@@ -0,0 +1,10 @@
+ import java.util.Scanner;
+
+public class Donation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int Y = scanner.nextInt();
+        System.out.println(Y - X);
+    }
+}
