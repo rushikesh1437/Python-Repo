2d5863b4d0ff0d0a0fc90953628ceb673b631c08
Rushikesh <147487623+rushikesh1437@users.noreply.github.com>
2024-10-30 12:09:58 +0530

    Uploading code: Count Divisors.py

---
+++ Count Divisors.py
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountMultiples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int l = scanner.nextInt();
+        int r = scanner.nextInt();
+        int k = scanner.nextInt();
+        int count = 0;
+        
+        for (int i = l; i <= r; i++) {
+            if (i % k == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+ 
+ 
